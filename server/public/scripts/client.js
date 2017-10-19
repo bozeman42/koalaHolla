3b8f20d01c23241cb6400d19a4abb64ab24344ac
David Beaudway <david.beaudway@gmail.com>
2017-10-19 12:01:04 -0500

    frontend

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,50 +1,68 @@
 console.log( 'js' );
 
-$( document ).ready( function(){
+$( document ).ready(readyNow);
+
+function readyNow(){
   console.log( 'JQ' );
   // load existing koalas on page load
   getKoalas();
-
   // add koala button click
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    var objectToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( objectToSend );
-  }); //end addButton on click
-}); // end doc ready
+  $( '#addButton' ).on( 'click', addButtonClick);
+}
 
+function addButtonClick(){
+  console.log( 'in addButton on click' );
+  var name = $('#nameIn').val();
+  var age = $('#ageIn').val();
+  var gender = $('#genderIn').val();
+  var readyForTransfer = $('#readyForTransferIn').val();
+  var notes = $('#notesIn').val();
+  // get user input and put in an object
+  // NOT WORKING YET :(
+  // using a test object
+  var objectToSend = {
+    name: name,
+    age: age,
+    gender: gender,
+    readyForTransfer: readyForTransfer,
+    notes: notes
+  }
+   // call saveKoala with the new obejct
+   saveKoala( objectToSend );
+}
+
+// GET AJAX call to server to get koalas
 function getKoalas(){
   console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
   $.ajax({
-    url: '/koalas',
     type: 'GET',
-    success: function( data ){
-      console.log( 'got some koalas: ', data );
-    } // end success
-  }); //end ajax
-  // display on DOM with buttons that allow edit of each
-} // end getKoalas
+    url: '/koalas'
+  }).done(function(response){
+    console.log( 'got some koalas: ', response );
+    //Append to dom function
+  appendToDom(response);
+  }).fail(function(error){
+    console.log('GET failed:', error);
+  })
+}
 
+// POST AJAX call to server to add new Koala
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
   $.ajax({
-    url: '/koalas',
     type: 'POST',
-    data: newKoala,
-    success: function( data ){
-      console.log( 'got some koalas: ', data );
-    } // end success
-  }); //end ajax
+    url: '/koalas',
+    data: newKoala
+  }).done(function(response){
+    console.log('Success:', response);
+    getKoalas();
+  }).fail(function(error){
+    console.log('Failed:',error);
+  })
+}
+
+function appendToDom(koalas) {
+  for(var i=0; i<koalas.length; i++){
+    $('#viewKoalas').append('<tr><td>' + koalas[i].name + '</td><td>' + koalas[i].age + '</td><td>' + koalas[i].gender + '</td><td>' + koalas[i].readyfortransfer + '</td><td>' + koalas[i].notes + '</td></tr>');
+  }
 }
